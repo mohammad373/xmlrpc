5eaf66c344a683afa7e2e16d3dc1c09185935c9f
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-11 13:38:21 +0330

    Update x.py

--- x.py
+++ x.py
@@ -10,7 +10,7 @@ def __xmlrpc__():
     os.system("clear")
     time.sleep(1)
 
-        print(Fore.YELLOW + "[" + Fore.BLUE + "*" + Fore,YELLOW + "]" + Fore.BLUE + " ~ " + Fore.GREEN + "Hello . Welcome Back ;)")
+        print(Fore.YELLOW + "[" + Fore.BLUE + "*" + Fore.YELLOW + "]" + Fore.BLUE + " ~ " + Fore.GREEN + "Hello . Welcome Back ;)")
         time.sleep(1)
         target = input(Fore.YELLOW + "\n[" + Fore.RED + "!" + Fore.YELLOW + "]" + Fore.RED + " ~ " + Fore.BLUE + "Pleass Enter The Address Target " + Fore.GREEN + "==>  ")
         time.sleep(1)
